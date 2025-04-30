844941ea1a2f90602ceeeaa50411057f17445e89
Manish Kumar <139037481+kmanishprogrammar@users.noreply.github.com>
2025-04-30 18:42:01 +0530

    combined code

---
+++ Milestone-3/task8/task8_part4/test4.py
@@ -0,0 +1,195 @@
+import streamlit as st
+from streamlit_tags import st_tags
+import pandas as pd
+import random
+import json
+import requests
+from datetime import datetime
+from markdownify import markdownify as md
+from bs4 import BeautifulSoup
+from pydantic import create_model
+import google.generativeai as genai
+
+USER_AGENTS  = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 13_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 Safari/605.1.15",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 Mobile/15E148 Safari/604.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"
+]
+
+system_prompt = """You are an intelligent text extraction and conversion assistant. Your task is to extract structured information
+                        from the given text and convert it into a pure JSON format. The JSON should contain only the structured data extracted from the text,
+                        with no additional commentary, explanations, or extraneous information.
+                        You could encounter cases where you can't find the data of the fields you have to extract or the data will be in a foreign language.
+                        Please process the following text and provide the output in pure JSON format with no words before or after the JSON:"""
+
+user_prompt = f"Extract the following information from the provided text:\nPage content:\n\n"
+
+
+def get_html_from_url(url):
+    headers = {
+        "User-Agent": random.choice(USER_AGENTS)
+    }
+    response = requests.get(url, headers=headers)
+    return response.text
+
+def clean_raw_data(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    if soup.header:
+        soup.header.decompose()
+    if soup.footer:
+        soup.footer.decompose()
+    return str(soup)
+
+def markdown(url):
+    html_content = get_html_from_url(url)
+    cleaned_html = clean_raw_data(html_content)
+    markdown_content = md(cleaned_html)
+    return markdown_content
+
+def divide_chunks(text, max_tokens):
+    words = text.split()
+    chunks = []
+    current_chunk = []
+    current_length = 0
+
+    for word in words:
+        word_length = len(word) + 1 
+        if current_length + word_length > max_tokens:
+            chunks.append(" ".join(current_chunk))
+            current_chunk = [word]
+            current_length = word_length
+        else:
+            current_chunk.append(word)
+            current_length += word_length
+
+    if current_chunk:
+        chunks.append(" ".join(current_chunk))
+
+    return chunks
+
+def process_chunks(chunks, system_prompt, user_prompt, model):
+    responses = []
+    for chunk in chunks:
+        prompt = f"{system_prompt} \n {user_prompt} {chunk}"
+        response = model.generate_content(prompt)
+        json_response = json.loads(response.text)
+        responses.extend(json_response["listings"])
+    return {"listings": responses}
+
+def pydantic_model(fields):
+    field_definitions = {field: (str, ...) for field in fields}
+    return create_model("DynamicListingModel", **field_definitions)
+
+def pydantic_container(model):
+    return create_model("DynamicListingsContainer", listings=(list[model], ...))
+
+def scrape(fields, url):
+    if fields:
+        dynamic_model = pydantic_model(fields)
+        dynamic_listing_container = pydantic_container(dynamic_model)
+        genai.configure(api_key="AIzaSyD5zflCCvz5dG6W-A-3SgZ-QSf-DNCdfjw")
+
+        model = genai.GenerativeModel("gemini-1.5-flash", generation_config={
+            "response_schema": dynamic_listing_container,
+            "response_mime_type": "application/json",
+            "temperature": 0,
+            "max_output_tokens": 8096})
+        raw_data = markdown(url)
+        chunks = list(divide_chunks(raw_data, 1800))
+        response = process_chunks(chunks, system_prompt, user_prompt, model)
+        return response
+
+def download_options(df):
+    csv = df.to_csv(index=False).encode("utf-8")
+    excel = io.BytesIO()
+    df.to_excel(excel, index=False, engine="xlsxwriter")
+    excel.seek(0)
+    json_data = df.to_json(orient="records")
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.download_button("Download as CSV", csv, "records.csv", "text/csv")
+    with col2:
+        st.download_button("Download as Excel", excel, "records.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    with col3:
+        st.download_button("Download as JSON", json_data, "records.json", "application/json")
+
+def ui():
+    st.set_page_config(layout="wide", page_title="Universal Web Scraper")
+    st.markdown("<h1 style='text-align: center;'>Universal Web Scraper 🦑</h1>", unsafe_allow_html=True)
+
+    with st.sidebar:
+        st.markdown("### Web Scraper Settings")
+        model = st.selectbox("Select Model", ["gemini flash", "gpt-4-mini", "gpt-3.5-turbo"])
+        url = st.text_input("Enter URL")
+        fields = st_tags(
+            label="Enter Fields to Extract:",
+            text="Press enter to add more",
+            suggestions=["name", "email", "address", "phone"],
+            maxtags=10,
+            key="1"
+        )
+        st.divider()
+        if st.button("Scrape"):
+            response = scrape(fields, url)
+            st.session_state['response'] = response
+
+    if 'response' in st.session_state:
+        st.success("Data Scraped successfully")
+        df = pd.DataFrame(st.session_state['response']["listings"])
+        st.dataframe(df)
+        download_options(df)
+
+if __name__ == "__main__":
+    ui()
